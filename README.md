5014254e92803e37dd4fe61779d84534053eaf08
Iñaki Úcar <iucar@fedoraproject.org>
2023-08-24 18:41:04 +0200

    initial categorization

---
+++ README.md
@@ -0,0 +1,10 @@
+# System Requirements for R Packages
+
+An attempt to categorize and systematize system requirements on Linux.
+
+## Structure
+
+- `types.csv`: Categories of system requirements, with name and description.
+- `sysreqs.csv`: System requirements, with name, type, and packages required
+  in different distributions.
+- `pkgdb.csv`: TODO.
---
+++ generate-spec.r
@@ -0,0 +1,40 @@
+#!/usr/bin/Rscript
+
+format_requires <- function(df) {
+  do.call(c, lapply(names(df), function(i) {
+    distros <- strsplit(i, "_")[[1]]
+    c(paste0("%if", paste0("0%{?", distros, "}", collapse=" || ")),
+      sprintf("Requires:       %s", na.omit(df[[i]])),
+      "%endif")
+  }))
+}
+
+replace_pattern <- function(x, pattern, replacement) {
+  idx <- grep(pattern, x)
+  c(x[1:(idx-1)], replacement, x[(idx+1):length(x)])
+}
+
+type <- read.csv("types.csv")
+sreq <- read.csv("sysreqs.csv", na.strings="")
+
+reqs <- c(
+  sprintf("Requires:       %%{name}-%-8s= %%{version}-%%{release}", type$name),
+  "", format_requires(subset(sreq, is.na(type))[,-(1:2)])
+)
+
+pkgs <- do.call(c, apply(type, 1, function(x) {
+  c(sprintf("%%package        %s", x["name"]),
+    sprintf("Summary:        %s", x["description"]),
+    "BuildArch:      noarch",
+    "", format_requires(subset(sreq, type==x["name"])[,-(1:2)]), "",
+    sprintf("%%description %s", x["name"]),
+    sprintf("%s.", x["description"]), "")
+}, simplify=FALSE))
+
+fils <- paste0("%files ", type$name, "\n", collapse="\n")
+
+readLines("R-build-deps.spec.in") |>
+  replace_pattern("@requires@", reqs) |>
+  replace_pattern("@subpackages@", pkgs) |>
+  replace_pattern("@files@", fils) |>
+  writeLines("R-build-deps.spec")
