490b09cca2aa48648876ed35965c6978b8f2d729
Iñaki Úcar <iucar@fedoraproject.org>
2023-08-24 21:13:42 +0200

    add build and run columns

--- generate-spec.r
+++ generate-spec.r
@@ -15,18 +15,18 @@ replace_pattern <- function(x, pattern, replacement) {
 }
 
 type <- read.csv("types.csv")
-sreq <- read.csv("sysreqs.csv", na.strings="")
+sreq <- read.csv("sysreqs.csv", na.strings="") |> subset(build)
 
 reqs <- c(
   sprintf("Requires:       %%{name}-%-8s= %%{version}-%%{release}", type$name),
-  "", format_requires(subset(sreq, is.na(type))[,-(1:2)])
+  "", format_requires(subset(sreq, is.na(type))[,-(1:4)])
 )
 
 pkgs <- do.call(c, apply(type, 1, function(x) {
   c(sprintf("%%package        %s", x["name"]),
     sprintf("Summary:        %s", x["description"]),
     "BuildArch:      noarch",
-    "", format_requires(subset(sreq, type==x["name"])[,-(1:2)]), "",
+    "", format_requires(subset(sreq, type==x["name"])[,-(1:4)]), "",
     sprintf("%%description %s", x["name"]),
     sprintf("%s.", x["description"]), "")
 }, simplify=FALSE))
